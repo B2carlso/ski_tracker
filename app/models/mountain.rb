5b564512122b6d8b1f13cfeb19c2f27dc77411c2
Brad Carlson <b2carlso@yahoo.com>
2018-03-31 16:38:57 +0000

    added associations

--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -1,2 +1,4 @@
 class Mountain < ApplicationRecord
+  belongs_to :user
+  has_many :trails
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :mountains
+  has_many :skis
+  has_many :days
+  has_many :trails, through: :mountains
+
 end

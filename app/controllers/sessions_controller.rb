a5d782ad40f2dcdf214173fdd12d69b7805adf9a
Brad Carlson <b2carlso@yahoo.com>
2018-04-07 21:28:15 +0000

    working on controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+     if params[:user] == nil || params[:user].empty?
+       redirect_to sessions_new_path
+     else
+     #elsif params[:user]
+       #return head(:forbidden) unless params[:password] == @user.password
+       #session[:user_id] = @user.id
+       redirect_to user_path(@user)
+     end
+   end
+
+private
+  def session_params
+    params.require(:user).permit(:name)
+  end
 end

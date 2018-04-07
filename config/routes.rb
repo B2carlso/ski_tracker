9e461b6d03040a562bc1236dc493b2a7604a809b
Brad Carlson <b2carlso@yahoo.com>
2018-04-07 21:28:41 +0000

    working on routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get    '/signin',   to: 'sessions#new'
+  post   '/signin',   to: 'sessions#create'
+
+  resources :users
 end

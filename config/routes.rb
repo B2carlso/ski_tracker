4106fd3ee08173fc2e14acbddcc02ec3d428f25f
Brad Carlson <b2carlso@yahoo.com>
2018-09-18 23:19:12 +0000

    updated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
   get    '/signin',   to: 'sessions#new'
   post   '/signin',   to: 'sessions#create'
 
